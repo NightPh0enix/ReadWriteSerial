a85b47e9c9c285d5efd443aa2f43f3938db940d9
Anshuman Singh <90270480+NightPh0enix@users.noreply.github.com>
2021-11-22 12:52:33 -0500

    Create serialRdWr.py
    This script does not need the user to manually check the COM port. The script automatically detects the COM port and opens it for rd/wr

---
+++ serialRdWr.py
@@ -0,0 +1,33 @@
+import serial
+import os
+import time
+import subprocess
+
+
+output = subprocess.check_output("python -m serial.tools.list_ports", shell=True) # sending command to terminal 
+output = output.decode('utf-8') # the output is of type b'somestring' This removes that
+output = output.splitlines() # so we can access the COM part 
+port =  output[0]
+
+data = open("Input.txt", "w")
+ser = serial.Serial(port, 115200, timeout=30)  # open serial port
+print("Serial Port opened on : " + ser.name)
+
+    
+
+ser.write(b'M564 S0 H0\r\n')
+time.sleep (0.02)
+ser.write(b'M111 S1 P6\r\n')
+time.sleep (0.02)
+ser.write(b'M111 S1 P4\r\n')
+
+counter = 0
+while (1):
+    response = ser.readline().decode('utf-8')
+    print(response)
+    data.write(response)
+    if response == "":
+        break
+
+time.sleep(1)
+data.close()
