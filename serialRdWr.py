1ef6986f16e46635de0e9e9be320637a93fa3f5c
Anshuman Singh <90270480+NightPh0enix@users.noreply.github.com>
2021-11-22 13:52:37 -0500

    Update serialRdWr.py

--- serialRdWr.py
+++ serialRdWr.py
@@ -7,8 +7,12 @@ import subprocess
 output = subprocess.check_output("python -m serial.tools.list_ports", shell=True) # sending command to terminal 
 output = output.decode('utf-8') # the output is of type b'somestring' This removes that
 output = output.splitlines() # so we can access the COM part 
-port =  output[0]
 
+# if on Linux add these lines and remove line 
+#     temp = output[0].split()
+#     port = temp[0]
+
+port =  output[0]
 data = open("Input.txt", "w")
 ser = serial.Serial(port, 115200, timeout=30)  # open serial port
 print("Serial Port opened on : " + ser.name)
